30dad22cb8188c4c6df057fcd6c7e0a3fcabfa71
lbedoyas <lucho.bedoya1@hotmail.com>
2019-04-18 19:02:35 -0500

    Install & conf HBS
    Se realiza instalacion de hbs y configuracion de index y creacion de carpetas de views

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 const path = require('path');
+const hbs = require('hbs');
 
 const directorioPublico = path.join(__dirname,'../public' );
 app.use(express.static(directorioPublico));
@@ -8,10 +9,17 @@ app.use(express.static(directorioPublico));
 
 console.log(__dirname)
 
+//Se traer los argumentos del hbs
+app.set('view engine', 'hbs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+
+app.get('/',(err,res)=>{
+    res.render('index',{
+        estudiante: 'Luis'
+    })
+});
+
+
  
 app.listen(3000,()=>{
     console.log("Escuchando el puerto 3000");
