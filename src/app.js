7959f128d7e35355ed80779b70b71e29cdf099de
lbedoyas <lucho.bedoya1@hotmail.com>
2019-04-18 19:47:53 -0500

    helpers
    Se realiza ejemplo de helpers con hbs con el requerimiento de promedio estudiante

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 const path = require('path');
 const hbs = require('hbs');
+require('./helpers');
 
 const directorioPublico = path.join(__dirname,'../public' );
 //Registramos la carpeta con los partials
@@ -25,8 +26,11 @@ app.get('/',(err,res)=>{
 
 app.get('/calculos',(req,res)=>{
     res.render('calculos',{
-        estudiante: 'Iris'
-    })
+        estudiante: 'Iris',
+        nota1: 4,
+        nota2: 5,
+        nota3: 4
+    });
 });
 
  
