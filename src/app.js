ce7d6e349ecc50316cf9e6daf3f91ed313acd4fc
lbedoyas <lucho.bedoya1@hotmail.com>
2019-04-19 09:52:24 -0500

    body-parser
    SE instala body parser y se crear formulario para ingreso de informacion de estudiantes por metodos post

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 const path = require('path');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 require('./helpers');
 
 const directorioPublico = path.join(__dirname,'../public' );
@@ -10,6 +11,8 @@ const directorioPartials = path.join(__dirname,'../partials');
 app.use(express.static(directorioPublico));
 //Se ejecutar el llamado indicando que aca estan los partials
 hbs.registerPartials(directorioPartials);
+//Traer argumentos de body-parser para usar
+app.use(bodyParser.urlencoded({extended: false}))
  
 
 console.log(__dirname)
@@ -18,20 +21,21 @@ console.log(__dirname)
 app.set('view engine', 'hbs');
 
 
+
 app.get('/',(err,res)=>{
     res.render('index',{
         estudiante: 'Luis'
     })
 });
 
-app.get('/calculos',(req,res)=>{
+app.post('/calculos',(req,res)=>{
 
     console.log(req.query);
     res.render('calculos',{
-        estudiante: req.query.nombre,
-        nota1: parseInt(req.query.nota1),
-        nota2: parseInt(req.query.nota2),
-        nota3: parseInt(req.query.nota3)
+        estudiante: req.body.nombre,
+        nota1: parseInt(req.body.nota1),
+        nota2: parseInt(req.body.nota2),
+        nota3: parseInt(req.body.nota3)
     });
 });
 
